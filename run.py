235ea08183cf007637f511e31cb1c63dd885d1c3
Eduardo-Antoniassi-Lobato <eduardoantlobato@gmail.com>
2023-03-22 11:32:57 +0000

    correction indentation

--- run.py
+++ run.py
@@ -13,8 +13,6 @@ logo = """
       `------'                           |__/           
 """
 
-print(logo)
-
 
 def deal_card():
     """Returns a random card from the deck."""
@@ -56,6 +54,8 @@ def compare(user_score, computer_score):
 
 def play_game():
 
+    print(logo)
+
     user_cards = []
     computer_cards = []
     is_game_over = False
@@ -64,8 +64,7 @@ def play_game():
         user_cards.append(deal_card())
         computer_cards.append(deal_card())
 
-
-while not is_game_over:
+    while not is_game_over:
 
         user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
@@ -78,23 +77,19 @@ while not is_game_over:
             user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
 
             if user_should_deal == "y":
-               user_cards.append(deal_card())
+                user_cards.append(deal_card())
             else:
-               is_game_over = True
+                is_game_over = True
 
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
 
-while computer_score != 0 and computer_score < 17:
-    computer_cards.append(deal_card())
-    computer_score = calculate_score(computer_cards)
+    print(f"   Your final hand: {user_cards}, final score: {user_score}")
+    print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
 
-print(f"   Your final hand: {user_cards}, final score: {user_score}")
-print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
-print(compare(user_score, computer_score))
 
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
     os.system('cls')
-    play_game()
-
-    
-
-
+    play_game() 
